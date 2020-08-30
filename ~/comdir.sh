2bdebc74e0bad2407c9727bf5d32e7df0589450d
Byambadalai Sumiya <byamba4life@gmail.com>
2020-08-30 02:35:56 +0000

    Create comdir.sh

---
+++ ~/comdir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Custom Directory changing commands
+# It's my path structure, so it might be different from yours
+
+function doc() {
+  cd ~/Documents/;
+}
+function dow() {
+  cd ~/Downloads/;
+}
+function bch() {
+  cd ~/Documents/MUST-Bachelor;
+}
+function ctf() {
+  cd ~/Documents/CTF;
+}
+function prj() {
+  cd ~/Documents/Projects;
+}
+function htb() {
+  cd ~/Documents/Hack-The-Box;
+}
+function thm() {
+  cd ~/Documents/Try-Hack-Me;
+}
