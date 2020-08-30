154bbcc12020fc12b19da101d8efcc5153e337de
Byambadalai Sumiya <byamba4life@gmail.com>
2020-08-30 03:14:54 +0000

    Create scrgit.sh

---
+++ scripts/scrgit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# I don't suggest run this script
+# This script clone my repositories. And some of them private.
+
+cd ~/Documents && git clone https://github.com/ByamB4/Algorithm-Problems-Solutions.git;
+cd ~/Documents && git clone https://github.com/ByamB4/MUST-Bachelor.git;
+cd ~/Documents/Projects && git clone https://github.com/OyuTech/OyuBook.git;
+
