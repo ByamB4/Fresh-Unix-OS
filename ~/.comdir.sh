9c43b45ffe258cfcf08b0d9c16e0f8e9f88db02b
Byambadalai Sumiya <byamba4life@gmail.com>
2021-09-16 14:58:00 +0800

    Update .comdir.sh

--- ~/.comdir.sh
+++ ~/.comdir.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 function doc() {
-  cd ~/Documents/;
+  cd ~/Documents;
 }
 function dow() {
-  cd ~/Downloads/;
-}
-function bch() {
-  cd ~/Documents/MUST-Bachelor;
+  cd ~/Downloads;
 }
 function ctf() {
-  cd ~/Documents/CTF;
+  cd ~/Documents/ctf;
 }
 function dev() {
-  cd ~/Documents/Developer;
+  cd ~/Documents/dev;
+}
+function tools() {
+  cd ~/Documents/ctf/tools;
+}
+function alg() {
+  cd ~/Documents/alg;
+}
+function games() {
+  cd ~/Documents/games;
 }
