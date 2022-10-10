cbe0c2969408b25a06629c1fef7f6e72c8c14caa
Byambadalai Sumiya <byamba4life@gmail.com>
2022-10-10 09:10:49 +0800

    Create .alias.sh

---
+++ home/.alias.sh
@@ -0,0 +1,8 @@
+alias gor='go run' 
+alias rm='rm -rf'
+alias brew86="arch -x86_64 /usr/local/bin/brew"
+alias pyenv86="arch -x86_64 pyenv"
+alias python3.6="/Users/l3yam134/.pyenv/versions/3.6.15/bin/python3.6"
+alias ssh-box='ssh root@10.5.118.3'
+alias ssh-aws='ssh -o StrictHostKeyChecking=no -i ~/.ssh/infosec-byamb4.pem ubuntu@ec2-13-214-174-48.ap-southeast-1.compute.amazonaws.com'
+alias ssh-me='ssh l3yam134@192.168.0.74'
